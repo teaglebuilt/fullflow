b9e4462da43dccade3e0da630522b65d5289b988
Dillan Teagle <dteagle1@LAMU02CPE9YMD6M.uhc.com>
2021-05-05 09:18:37 -0500

    add cli tests

--- tests/unit/cli/test_project.py
+++ tests/unit/cli/test_project.py
@@ -3,9 +3,7 @@ import sys
 from fullflow.cli import cli
 
 
-
-def test_project_created_with_template(runner, fake_root_dir):
-    sys.path = [str(fake_root_dir), str(fake_root_dir / "src")] + sys.path
-    result = runner.invoke(cli, ["init", "project_test", "--template", "kubernetes"])
-    assert os.path.isdir(f"{fake_root_dir}/project_test")
-    assert result.exit_code == 0
+def test_project_created_with_template(runner):
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, ["init", "project_test", "--template", "kubernetes"])
+        assert result.exit_code == 0
